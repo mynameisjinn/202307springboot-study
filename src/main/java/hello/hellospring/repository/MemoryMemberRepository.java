934d05a444876899cd740b7cfcd1b3e68fd35240
hyejin <obndo8539@naver.com>
2023-07-06 16:29:06 +0900

    ''

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,53 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+
+        /* 화살표 함수를 풀어서 작성
+        store.values().stream()
+                .filter(new Predicate<Member>() {
+                    @Override
+                    public boolean test(Member member) {
+                        return member.getName().equals(name);
+                    }
+                })
+                .findAny();
+         */
+
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
