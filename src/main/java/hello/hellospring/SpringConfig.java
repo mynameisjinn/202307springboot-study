1900d89017e5910480d2bb6fc9e0ef4f1b2fe3c6
hyejin <obndo8539@naver.com>
2023-07-07 16:54:24 +0900

    스프링 데이터 JAP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -11,18 +11,12 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        return new MemberService(memberRepository);
     }
 }
